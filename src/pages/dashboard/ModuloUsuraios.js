97807f80ae259fa44648cf7dec52b26bf94f7e49
sostraik1105 <83390692+sostraik1105@users.noreply.github.com>
2022-07-14 03:13:51 -0500

    quiero dormir

--- src/pages/dashboard/ModuloUsuraios.js
+++ src/pages/dashboard/ModuloUsuraios.js
@@ -2,20 +2,6 @@ import React from "react";
 import FormUsuarios from "../../components/usuarios/formUsuarios/FormUsuarios";
 import ListaUsuarios from "../../components/usuarios/listaUsuarios/ListaUsuarios";
 
-{
-  /* <div class="container-sm">
-      <h1>Usuarios</h1>
-      <div class="row">
-        <div class="col">
-          <FormUsuarios />
-        </div>
-        <div class="col">
-          <ListaUsuarios />
-        </div>
-      </div>
-    </div> */
-}
-
 export const ModuloUsuraios = () => {
   return (
     <div class="container-sm">
